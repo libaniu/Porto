f23da7d7d7c5efd414ba737b97825c117bf2809c
Fahmi <fahminabill40@gmail.com>
2026-04-26 13:56:44 +0700

    pause

--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export function middleware() {
-  return new NextResponse('Situs ini sedang tidak tersedia.', { status: 503 })
-}
-
-export const config = {
-  matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)',
-}
